02c2ffe97154a9878bad9418ca3b593a23f00c96
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-09-11 15:36:25 -0500

    updates

--- app/factories/getEventsFactory.js
+++ app/factories/getEventsFactory.js
@@ -1,5 +1,18 @@
 "use strict";
 
 app.factory("getEvents", function($q, $http){
-
+    app.factory("getEventsFactory", function($q, $http, FBCreds){
+        
+        const getAllEvents = function(){
+            return $q((resolve, reject)=>{
+                $http.get(`${FBCreds.databaseURL}/submitted-events.json`)
+                .then((events)=>{
+                    let allEvents = events.data;
+                    console.log(allEvents);
+                    resolve(allEvents);
+                });
+            });
+            //make call to firebase to get the user's profile info
+        };
+    });
 });
