cbacdfcee294f321f081dc9f75c094963463d5d4
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2017-10-14 15:33:07 -0500

    Create fb.creds.js

---
+++ app/creds/fb.creds.js
@@ -0,0 +1,13 @@
+"use strict";
+// console.log("FB Creds Yo!");
+
+//GET THIS FROM FIREBASE OVERVIEW >> Add Firebase to your web app
+app.constant("FBCreds", {
+    apiKey: "AIzaSyAG8yM25q_DRag39_Qw4zAACsMRAlNjoxA",
+    authDomain: "front-end-capstone-ce3ec.firebaseapp.com",
+    databaseURL: "https://front-end-capstone-ce3ec.firebaseio.com",
+    projectId: "front-end-capstone-ce3ec",
+    storageBucket: "front-end-capstone-ce3ec.appspot.com",
+    messagingSenderId: "622087774865"
+});
+
