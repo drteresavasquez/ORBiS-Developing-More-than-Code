93662dd1489e50c4756c17ecd99b133d684dbb3b
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-10-14 16:39:54 -0500

    stash

---
+++ app/deployment/pointstest.js
@@ -0,0 +1,16 @@
+// Commands for GH-pages
+
+// First, fork the repository and save it to your account.
+
+// Next, clone the fork you just created by typing this command in Git Shell:
+
+// git clone https://github.com/<your github username>/angular.git
+// Then, make a branch called github pages and switch to it.
+
+// cd angular/
+// git branch gh-pages
+// git checkout gh-pages
+// Last but not least, push the new branch to Github by setting a new upstream.
+
+// git push --set-upstream origin gh-pages
+// You’re done! You can see your result at http://<your github username>.github.io/angular.
